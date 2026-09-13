8ee119ea5d2bd5bd8e9307cc4a2e6ae1c85f2b96
Maikeru123 <michaelvelez112301@gmail.com>
2026-09-13 20:28:18 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_crud3/HomePage.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase CRUD',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: HomePage(),
+    );
+  }
+}
